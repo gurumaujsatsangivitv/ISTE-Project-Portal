688f965377b5b8c22da354c2ca04bd8da7d34866
gurumaujsatsangivitv <gurumauj.satsangi2023@vitstudent.ac.in>
2024-10-26 10:08:56 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import BentoGridDemo from './components/test/bento'
+
+function App() {
+  return (
+    <>
+      <BentoGridDemo />
+    </>
+  );
+}
+export default App
