379f966929bbca584070e462e44ce94cd11c35f8
gurumaujsatsangivitv <gurumauj.satsangi2023@vitstudent.ac.in>
2024-12-17 21:31:59 +0530

    Update header.jsx

--- src/components/test/header.jsx
+++ src/components/test/header.jsx
@@ -4,9 +4,9 @@ import Navbar from './navbar/Navbar.jsx';
 
 const Header = ({ setCurrentPage }) => {
   return (
-    <div className="w-screen h-[580px] bg-black rounded-b-[20px] opacity-100 flex items-center justify-center text-center relative">
+    <div className="w-screen h-[580px] bg-black rounded-b-[20px] opacity-100 flex flex-col items-center justify-center text-center relative px-4">
       <Navbar setCurrentPage={setCurrentPage} />
-      <h1 className="text-white text-7xl mt-0">
+      <h1 className="text-white text-5xl md:text-6xl lg:text-7xl mt-4">
         <span className="font-VanioTrial">welcome to the</span> <br />
         <span className="font-George font-bold">ISTE Projects Portal</span>
       </h1>
@@ -15,3 +15,5 @@ const Header = ({ setCurrentPage }) => {
 };
 
 export default Header;
+
+
