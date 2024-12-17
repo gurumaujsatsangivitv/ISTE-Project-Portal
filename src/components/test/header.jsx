8e80ffbfec4fda814aca18aba362659b40fdaae7
Sara Sharma <sara.sharma1135@gmail.com>
2024-12-18 01:23:28 +0530

    responsive font size header.jsx

--- src/components/test/header.jsx
+++ src/components/test/header.jsx
@@ -4,9 +4,9 @@ import Navbar from './navbar/Navbar.jsx';
 
 const Header = ({ setCurrentPage }) => {
   return (
-    <div className="w-screen h-[580px] bg-black rounded-b-[20px] opacity-100 flex flex-col items-center justify-center text-center relative px-4">
+    <div className="w-screen  h-[520px] md:h-[580px]  bg-black rounded-b-[20px] opacity-100 flex flex-col items-center justify-center text-center relative">
       <Navbar setCurrentPage={setCurrentPage} />
-      <h1 className="text-white text-5xl md:text-6xl lg:text-7xl mt-4">
+      <h1 className="text-white text-5xl mt-0 md:text-7xl mt-0">
         <span className="font-VanioTrial">welcome to the</span> <br />
         <span className="font-George font-bold">ISTE Projects Portal</span>
       </h1>
